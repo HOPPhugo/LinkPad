9987b63d0d07c6d697d19f78912bfdb1cbfa609c
Hugo Schweizer <hugschweize@etmlnet.local>
2025-05-13 08:14:02 +0200

    On peut accéder aux jeux suivant : TryToEscape. Bataille navale, Strange Village, Chronomètre ULTIME

---
+++ LinkPad/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LinkPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(258, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenue dans le --LinkPad--";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(219, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(357, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Choisissez le jeu/application que vous souhaitez utiliser\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(309, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "1. TryToEscape!";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(300, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "3. Strange Village";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(291, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(225, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "4. Chronomètre ULTIME";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(310, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "2.Bataille Navale";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ LinkPad/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkPad
+{
+    public partial class Form1 : Form
+    {
+        string path = AppDomain.CurrentDomain.BaseDirectory; // récupère le chemin d'accès de mon jeu ( je l'utilise pour crée et trouver les fichier pour les events)
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Process jeuProcess = new Process();
+            jeuProcess.StartInfo.FileName = path + @"\Try_To_ESCAPE_ GAME.exe";
+            jeuProcess.Start();
+            return;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Process jeuProcess = new Process();
+            jeuProcess.StartInfo.FileName = path + @"\Idle Game --Strange Village--";
+            jeuProcess.Start();
+            return;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Process jeuProcess = new Process();
+            jeuProcess.StartInfo.FileName = path + @"\Chronomètre Ultime.exe";
+            jeuProcess.Start();
+            return;
+            
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+            Process jeuProcess = new Process();
+            jeuProcess.StartInfo.FileName = path + @"\bataille navale escape.exe";
+            jeuProcess.Start();
+            return;
+        }
+    }
+}
